c42178ed3cd01723dca511ae394f169c42f9704e
Diego Leite <diego@DESKTOP-ME841EH>
2022-02-02 21:13:48 -0300

    criacao classes Employee e OutsourcedEmployee

---
+++ heranca_exercicio01/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heranca_exercicio01.Entities {
+    class OutsourcedEmployee : Employee {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours,valuePerHour) {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double payment() {
+            double payment = ValuePerHour * Hours;
+            double result;
+            return result = payment + (payment * (AdditionalCharge/100));
+        }
+    }
+}
